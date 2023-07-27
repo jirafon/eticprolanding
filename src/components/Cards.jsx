52e7566bc18d108c73ad726840ef5726e93c4a5a
Jirafon <chaquin@gmail.com>
2023-07-27 17:43:34 -0400

    22

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,48 +1,59 @@
 import React from 'react';
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+import Single from '../assets/single.png';
+import Double from '../assets/double.png';
+import Triple from '../assets/triple.png';
 
 const Cards = () => {
   return (
     <div className='w-full py-[10rem] px-4 bg-white'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
-          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>SMB</h2>
-              <p className='text-center text-4xl font-bold'>30 UF / Anual</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>400 Declaraciones</p>
-                  <p className='py-2 border-b mx-8'>2 Usuarios</p>
-                  <p className='py-2 border-b mx-8'>2 Campañas</p>
+      <div className='max-w-[1240px] mx-auto grid grid-cols-4 gap-8'>
+        <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+          <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
+          <h2 className='text-2xl font-bold text-center py-8'>Small</h2>
+          <p className='text-center text-4xl font-bold'>30 UF / Anual</p>
+          <div className='text-center font-medium'>
+            <p className='py-2 border-b mx-8 mt-8'>400 Declaraciones</p>
+            <p className='py-2 border-b mx-8'>2 Usuarios</p>
+            <p className='py-2 border-b mx-8'>2 Campañas</p>
+          </div>
+        </div>
 
-                 
-              </div>
-           {/*}   <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Comenzar</button> */}
+        <div className='w-full sm:w-auto shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+          <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
+          <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
+          <h2 className='text-2xl font-bold text-center py-8'>Mid</h2>
+          <p className='text-center text-4xl font-bold'>60 UF / Anual</p>
+          <div className='text-center font-medium'>
+            <p className='py-2 border-b mx-8 mt-8'>800 Declaraciones</p>
+            <p className='py-2 border-b mx-8'>2 Usuarios</p>
+            <p className='py-2 border-b mx-8'>2 Campañas</p>
+            <p className='py-2 border-b mx-8'>1 Licencias E-learning </p>
           </div>
-          <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Pro</h2>
-              <p className='text-center text-4xl font-bold'>132,5 UF / Anual</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>2000 Declaraciones</p>
-                  <p className='py-2 border-b mx-8'>5 Usuarios</p>
-                  <p className='py-2 border-b mx-8'>2 Campañas</p>
-                  <p className='py-2 border-b mx-8'>2 Licencias E-learning </p>
-              </div>
-          </div>
-          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Enterprise</h2>
-              <p className='text-center text-4xl font-bold'>285 UF / Anual</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>5000 Declaraciones</p>
-                  <p className='py-2 border-b mx-8'>Usuarios Ilimitado</p>
-                  <p className='py-2 border-b mx-8'>8 Campañas</p>
-                  <p className='py-2 border-b mx-8'> 5 Licencias E-learning</p>
+        </div>
 
-              </div>
+        <div className='w-full sm:w-auto shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+          <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
+          <h2 className='text-2xl font-bold text-center py-8'>Pro</h2>
+          <p className='text-center text-4xl font-bold'>132,5 UF / Anual</p>
+          <div className='text-center font-medium'>
+            <p className='py-2 border-b mx-8 mt-8'>2000 Declaraciones</p>
+            <p className='py-2 border-b mx-8'>5 Usuarios</p>
+            <p className='py-2 border-b mx-8'>2 Campañas</p>
+            <p className='py-2 border-b mx-8'>2 Licencias E-learning </p>
           </div>
+        </div>
+
+        <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+          <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
+          <h2 className='text-2xl font-bold text-center py-8'>Enterprise</h2>
+          <p className='text-center text-4xl font-bold'>285 UF / Anual</p>
+          <div className='text-center font-medium'>
+            <p className='py-2 border-b mx-8 mt-8'>5000 Declaraciones</p>
+            <p className='py-2 border-b mx-8'>Usuarios Ilimitado</p>
+            <p className='py-2 border-b mx-8'>8 Campañas</p>
+            <p className='py-2 border-b mx-8'>5 Licencias E-learning</p>
+          </div>
+        </div>
       </div>
     </div>
   );
