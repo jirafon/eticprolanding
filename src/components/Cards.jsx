c4fcc2e31d86edaf0a95447c27dd09af06e69650
Jirafon <chaquin@gmail.com>
2023-08-10 22:07:41 -0400

    75

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Single from '../assets/single.png';
 import Double from '../assets/double.png';
 import Triple from '../assets/triple.png';
+import Multi from '../assets/multi.png';
 
 const Cards = () => {
   return (
@@ -16,23 +17,35 @@ const Cards = () => {
             <p className='py-2 border-b mx-8 mt-8'>400 Declaraciones</p>
             <p className='py-2 border-b mx-8'>2 Usuarios</p>
             <p className='py-2 border-b mx-8'>2 Campañas</p>
+            <p className='py-2 border-b mx-8'>Sin Licencias E-learning </p>
+
+            <p className='py-2 border-b mx-8'>Registros de Reuniones Ilimitado</p>
+            <p className='py-2 border-b mx-8'>Gestíon de Denuncias Anónimas Ilimitado</p>
+
+
           </div>
         </div>
 
         {/* Mid Card */}
         <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+        <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Double} alt="double" />
+
           <h2 className='text-2xl font-bold text-center py-8'>Mid</h2>
           <p className='text-center text-4xl font-bold'>70 UF / Anual</p>
           <div className='text-center font-medium'>
             <p className='py-2 border-b mx-8 mt-8'>1000 Declaraciones</p>
             <p className='py-2 border-b mx-8'>2 Usuarios</p>
             <p className='py-2 border-b mx-8'>2 Campañas</p>
-            <p className='py-2 border-b mx-8'>1 Licencias E-learning </p>
+            <p className='py-2 border-b mx-8'>1 Licencia E-learning </p>
+            <p className='py-2 border-b mx-8'>Registros de Reuniones Ilimitado</p>
+            <p className='py-2 border-b mx-8'>Gestíon de Denuncias Anónimas Ilimitado</p>
           </div>
         </div>
 
         {/* Pro Card */}
         <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+        <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="triple" />
+
           <h2 className='text-2xl font-bold text-center py-8'>Pro</h2>
           <p className='text-center text-4xl font-bold'>132 UF / Anual</p>
           <div className='text-center font-medium'>
@@ -40,11 +53,15 @@ const Cards = () => {
             <p className='py-2 border-b mx-8'>5 Usuarios</p>
             <p className='py-2 border-b mx-8'>2 Campañas</p>
             <p className='py-2 border-b mx-8'>2 Licencias E-learning </p>
+            <p className='py-2 border-b mx-8'>Registros de Reuniones Ilimitado</p>
+            <p className='py-2 border-b mx-8'>Gestíon de Denuncias Anónimas Ilimitado</p>
           </div>
         </div>
 
         {/* Enterprise Card */}
         <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+        <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Multi} alt="multi" />
+
           <h2 className='text-2xl font-bold text-center py-8'>Enterprise</h2>
           <p className='text-center text-4xl font-bold'>285 UF / Anual</p>
           <div className='text-center font-medium'>
@@ -52,6 +69,8 @@ const Cards = () => {
             <p className='py-2 border-b mx-8'>Usuarios Ilimitado</p>
             <p className='py-2 border-b mx-8'>8 Campañas</p>
             <p className='py-2 border-b mx-8'>5 Licencias E-learning</p>
+            <p className='py-2 border-b mx-8'>Registros de Reuniones Ilimitado</p>
+            <p className='py-2 border-b mx-8'>Gestíon de Denuncias Anónimas Ilimitado</p>
           </div>
         </div>
       </div>
