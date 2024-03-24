91ad3cfd5da0945911c34a3ea3e9581923e3d10b
Jirafon <chaquin@gmail.com>
2024-03-24 14:00:28 -0300

    100

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -6,24 +6,20 @@ const Analytics = () => {
     <div className='w-full bg-white py-16 px-4'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-1'>
         <div className='flex flex-col items-center justify-center'>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>¿Qué es Eticpro?
-</h1>
-       <div style={{ position: 'relative', paddingBottom: '39.375%', height: 0, overflow: 'hidden', width: '70%' }}>
-  <iframe
-    allow="autoplay; gyroscope;"
-    allowfullscreen
-    height="100%"
-    referrerpolicy="strict-origin"
-src="https://www.kapwing.com/e/65931ff121b875ab8f832aa1"
-    style={{ border: '0', height: '100%', left: '0', position: 'absolute', top: '0', width: '100%' }}
-    title="Embedded content made on Kapwing"
-    width="100%"
-  ></iframe>
-  </div>  
-
-
-</div>
-
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>¿Qué es Eticpro?</h1>
+          <div style={{ position: 'relative', paddingBottom: '39.375%', height: 0, overflow: 'hidden', width: '70%' }}>
+            <iframe
+              allow="autoplay; gyroscope; fullscreen;"
+              allowfullscreen
+              height="100%"
+              referrerpolicy="strict-origin"
+              src="https://www.kapwing.com/e/65931ff121b875ab8f832aa1"
+              style={{ border: '0', height: '100%', left: '0', position: 'absolute', top: '0', width: '100%' }}
+              title="Video Eticpro"
+              width="100%"
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   );
