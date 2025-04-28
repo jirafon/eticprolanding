f28a30aa2f0bc433af302178546e9d6c81faa76b
Jirafon <chaquin@gmail.com>
2025-04-28 19:12:45 -0400

    d

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -161,7 +161,7 @@ const DemoModal = ({ isOpen, onClose }) => {
             >
               <FontAwesomeIcon icon={faTimes} />
             </button>
-            <h2 className='text-3xl font-bold mb-6 text-center'>Prueba Eticpro</h2>
+            <h2 className='text-3xl font-bold mb-6 text-center'>Contactenme</h2>
             <p className='text-2xl md:text-lg mb-6 text-center'>
               Llena el siguiente formulario y un representante te contactará a la brevedad.
             </p>
