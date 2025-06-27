80876aead1eedfa92778a75664056951eb041ae5
Jirafon <chaquin@gmail.com>
2025-06-26 23:10:26 -0400

    Fix Render deploy: remove react-snapshot, add _redirects for SPA, clean imports

--- src/App.js
+++ src/App.js
@@ -3,36 +3,20 @@ import React, { useState, useRef } from 'react';
 import Analytics from './components/Analytics';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Newsletter from './components/Newsletter';
 import About from './components/About';
-import Description from './components/Description';
-import Description1 from './components/Description1';
 import Features from './components/Features';
-import CardsPdm from './components/CardsPdm';
-import CardsKb from './components/CardsKb';
 import Cards from './components/Cards';
-
 import Help from './components/Help';
 import Centre from './components/centre';
 import News from './components/News';
-import Blog from './components/Blog';
-import Partner from './components/Partner';
-
 import Clients from './components/Clients';
-import Chatbot from './components/Chat';
-import CardsDr from './components/CardsDr';
-
 
 function App() {
-  const [showNewsletter, setShowNewsletter] = useState(false);
-  const [animationClass, setAnimationClass] = useState('');
   const aboutRef = useRef(null);
 
   const handleContactClick = () => {
-    setShowNewsletter(true);
-    setAnimationClass('animate__slideInRight'); // or 'animate__slideInLeft' for left animation
+    // Newsletter functionality can be added here if needed
   };
 
   const handleAboutClick = () => {
@@ -40,34 +24,19 @@ function App() {
   };
 
   return (
-
     <div id="inicio" className="inicio" style={{ marginTop: '5%' }}>
-      
       <Navbar handleContactClick={handleContactClick} handleAboutClick={handleAboutClick} />
       <Analytics />
       <Help />
-
       <Centre />
-      
       <Features />
-
       <About />
-      
-      {/* <Cards /> */}
-    <Cards />
-   
-    <Clients />
-    <Faq />
-    <News />
-
-    
-
+      <Cards />
+      <Clients />
+      <Faq />
+      <News />
       <Footer />
-    
     </div>
-    
-
-    
   );
 }
 
