bd5b3dc65ef3377dc0605eca8e1b0b58c9debe5a
Jirafon <chaquin@gmail.com>
2025-10-08 15:03:41 -0300

    Reducir tamaño texto Contáctenos en tarjeta CUSTOM

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -81,7 +81,7 @@ const Cards = () => {
         <div className='w-full sm:w-auto shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
           <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Multi} alt="custom" />
           <h2 className='text-2xl font-bold text-center py-8'>CUSTOM / A&nbsp;LA&nbsp;MEDIDA</h2>
-          <p className='text-center text-4xl font-bold'>Contáctenos</p>
+          <p className='text-center text-2xl font-bold'>Contáctenos</p>
           <div className='text-center font-medium'>
             <p className='py-2 border-b mx-8'>Desarrollo y módulos adicionales</p>
             <p className='py-2 border-b mx-8'>Integraciones personalizadas</p>
